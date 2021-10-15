2c16c9b268e4d61fcbea9e124f47701362663379
邓捷 <dj@lifesdk.com>
2021-10-15 11:17:01 +0800

    Import sql

---
+++ sql/docker-entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+CONSUL_HTTP_ADDR=${ENV_CONSUL_HOST}:${ENV_CONSUL_PORT} consul services register -address=apollo-configservice.${ENV_CLUSTER_NAMESPACE}.svc.cluster.local -name=apollo-configservice.npool.top -port=8080
+if [ ! $? -eq 0 ]; then
+  echo "FAIL TO REGISTER ME TO CONSUL"
+  exit 1
+fi
+
+MYSQL_HOST=`curl http://${ENV_CONSUL_HOST}:${ENV_CONSUL_PORT}/v1/agent/service/mysql.npool.top | jq .Address`
+if [ ! $? -eq 0 ]; then
+  echo "FAIL TO GET MYSQL HOST"
+  exit 1
+fi
+
+MYSQL_PORT=`curl http://${ENV_CONSUL_HOST}:${ENV_CONSUL_PORT}/v1/agent/service/mysql.npool.top | jq .Port`
+if [ ! $? -eq 0 ]; then
+  echo "FAIL TO GET MYSQL PORT"
+  exit 1
+fi
+
+MYSQL_HOST=`echo $MYSQL_HOST | sed 's/"//g'`
+export SPRING_DATASOURCE_URL=jdbc:mysql://$MYSQL_HOST:$MYSQL_PORT/ApolloConfigDB?characterEncoding=utf8&createDatabaseIfNotExist=true&useSSL=false&autoReconnect=true&useUnicode=true&user=root&password=$MYSQL_PASSWORD
+
+mysql -uroot -p$MYSQL_PASSWORD -h$MYSQL_HOST < /apolloconfigdb.sql
+mysql -uroot -p$MYSQL_PASSWORD -h$MYSQL_HOST < /apolloportaldb.sql
+if [ ! $? -eq 0 ]; then
+  echo "FAIL TO IMPORT SQL FILE"
+  exit 1
+fi
+
