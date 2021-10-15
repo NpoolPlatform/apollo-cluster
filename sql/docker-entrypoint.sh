fd1d865eb0e40d725ef176b566ef4abdaf8fbab1
邓捷 <dj@lifesdk.com>
2021-10-15 11:32:10 +0800

    bugfix

--- sql/docker-entrypoint.sh
+++ sql/docker-entrypoint.sh
@@ -13,9 +13,6 @@ if [ ! $? -eq 0 ]; then
   exit 1
 fi
 
-MYSQL_HOST=`echo $MYSQL_HOST | sed 's/"//g'`
-export SPRING_DATASOURCE_URL=jdbc:mysql://$MYSQL_HOST:$MYSQL_PORT/ApolloConfigDB?characterEncoding=utf8&createDatabaseIfNotExist=true&useSSL=false&autoReconnect=true&useUnicode=true&user=root&password=$MYSQL_PASSWORD
-
 mysql -uroot -p$MYSQL_PASSWORD -h $MYSQL_HOST < /apolloconfigdb.sql
 mysql -uroot -p$MYSQL_PASSWORD -h $MYSQL_HOST < /apolloportaldb.sql
 if [ ! $? -eq 0 ]; then
