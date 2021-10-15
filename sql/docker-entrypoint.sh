312cf02812f73d125d547eae63a9702a934e0279
邓捷 <dj@lifesdk.com>
2021-10-15 11:31:07 +0800

    bugfix

--- sql/docker-entrypoint.sh
+++ sql/docker-entrypoint.sh
@@ -16,8 +16,8 @@ fi
 MYSQL_HOST=`echo $MYSQL_HOST | sed 's/"//g'`
 export SPRING_DATASOURCE_URL=jdbc:mysql://$MYSQL_HOST:$MYSQL_PORT/ApolloConfigDB?characterEncoding=utf8&createDatabaseIfNotExist=true&useSSL=false&autoReconnect=true&useUnicode=true&user=root&password=$MYSQL_PASSWORD
 
-mysql -uroot -p$MYSQL_PASSWORD -h$MYSQL_HOST < /apolloconfigdb.sql
-mysql -uroot -p$MYSQL_PASSWORD -h$MYSQL_HOST < /apolloportaldb.sql
+mysql -uroot -p$MYSQL_PASSWORD -h $MYSQL_HOST < /apolloconfigdb.sql
+mysql -uroot -p$MYSQL_PASSWORD -h $MYSQL_HOST < /apolloportaldb.sql
 if [ ! $? -eq 0 ]; then
   echo "FAIL TO IMPORT SQL FILE"
   exit 1
