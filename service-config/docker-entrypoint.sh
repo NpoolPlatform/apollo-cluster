660cbee0615ec68748605c3c9bc229f8aecaa17f
Zhao KK <kikakkz@hotmail.com>
2021-10-29 18:07:06 +0000

    Add quote

--- service-config/docker-entrypoint.sh
+++ service-config/docker-entrypoint.sh
@@ -19,19 +19,16 @@ MYSQL_PORT=`curl http://${ENV_CONSUL_HOST}:${ENV_CONSUL_PORT}/v1/agent/health/se
 if [ ! $? -eq 0 ]; then
   echo "FAIL TO GET MYSQL PORT"
   exit 1
-else
-  echo "debug info: $MYSQL_HOST:$MYSQL_PORT"
 fi
 
 MYSQL_HOST=`echo $MYSQL_HOST | sed 's/"//g'`
-export SPRING_DATASOURCE_URL=jdbc:mysql://$MYSQL_HOST:$MYSQL_PORT/ApolloConfigDB?characterEncoding=utf8&createDatabaseIfNotExist=true&useSSL=false&autoReconnect=true&useUnicode=true
+export SPRING_DATASOURCE_URL="jdbc:mysql://$MYSQL_HOST:$MYSQL_PORT/ApolloConfigDB?characterEncoding=utf8&createDatabaseIfNotExist=true&useSSL=false&autoReconnect=true&useUnicode=true"
 
 
 mysql -uroot -p$MYSQL_PASSWORD -h $MYSQL_HOST < /apolloconfigdb.sql
 mysql -uroot -p$MYSQL_PASSWORD -h $MYSQL_HOST < /apolloportaldb.sql
 if [ ! $? -eq 0 ]; then
   echo "FAIL TO IMPORT SQL FILE with options $MYSQL_HOST $MYSQL_PORT"
-  # exit 1
 fi
 
 
